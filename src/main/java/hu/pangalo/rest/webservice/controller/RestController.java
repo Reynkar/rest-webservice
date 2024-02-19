70e925175d6434a75402607fde049eaf49d9d287
Reynkar <Cj2dkRb4>
2024-02-19 11:42:12 +0100

    feat: added new endpoints to test latened response

--- src/main/java/hu/pangalo/rest/webservice/controller/RestController.java
+++ src/main/java/hu/pangalo/rest/webservice/controller/RestController.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -24,5 +26,21 @@ public class RestController {
 		Integer counter = counterService.incrementCounter();
 		logger.info("counter: " + counter);
 		return ResponseEntity.ok("Hello World! in-memory counter: " + counter);
+	}
+
+	@GetMapping(path = "/latened-response")
+	public ResponseEntity<String> latenedResponse() throws InterruptedException {
+		Integer latency = counterService.getLatencyInSeconds();
+		logger.info("entered latened reponse, latency in seconds: " + latency);
+		Thread.sleep(latency.intValue() * 1000);
+		logger.info("latened reponse calling finished");
+		return ResponseEntity.ok("latened reponse calling finished");
+	}
+
+	@PostMapping(path = "/latency/{latency}")
+	public ResponseEntity<String> setLatency(@PathVariable Integer latency) {
+		counterService.setLatencyInSeconds(latency);
+		logger.info("latency set to " + latency + " seconds!");
+		return ResponseEntity.ok("latency set to " + latency + " seconds!");
 	}
 }
--- src/main/java/hu/pangalo/rest/webservice/service/CounterService.java
+++ src/main/java/hu/pangalo/rest/webservice/service/CounterService.java
@@ -7,7 +7,17 @@ public class CounterService {
 
 	private Integer counter = 0;
 
+	private Integer latencyInSeconds = 0;
+
 	public Integer incrementCounter() {
 		return ++counter;
+	}
+
+	public Integer getLatencyInSeconds() {
+		return latencyInSeconds;
+	}
+
+	public void setLatencyInSeconds(Integer latencyInSeconds) {
+		this.latencyInSeconds = latencyInSeconds;
 	}
 }
